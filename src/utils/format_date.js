b81e86f7028ff8022b9b65b87bd8c95651836dbb
Martin Crawford <mcrawford@touchbistro.com>
2022-04-30 17:04:31 -0400

    Fix: Date timezone issue.

--- src/utils/format_date.js
+++ src/utils/format_date.js
@@ -2,5 +2,8 @@ const DATE_FORMAT_OPTS = { year: "numeric", month: "long", day: "numeric" };
 
 export function toFormatDate(date) {
   // Ensure the ISO date string is treated in the Eastern Standard Timezone
-  return new Date(date + "T00:00:00-05:00").toLocaleDateString("fr-CA", DATE_FORMAT_OPTS);
+  return new Date(date + "T00:00:00-05:00").toLocaleDateString(
+    "fr-CA",
+    DATE_FORMAT_OPTS
+  );
 }
